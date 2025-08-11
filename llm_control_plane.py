c59871cd30f54a96c562733f0122f026c867762e
Yifei Zheng <zyf0717@gmail.com>
2025-08-11 16:27:09 +0800

    Updated for cron job

--- llm_control_plane.py
+++ llm_control_plane.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=12340, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=12340, reload=False)
